07a9c720ffcd889b5e66759ae78726038f8d018d
Kato Shinya <kato.shinya.dev@gmail.com>
2021-02-14 16:44:59 +0900

    add API document

---
+++ app/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"org.thinkit.zenna.annotation"},{"l":"org.thinkit.zenna.catalog"},{"l":"org.thinkit.zenna.exception"},{"l":"org.thinkit.zenna.util"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
