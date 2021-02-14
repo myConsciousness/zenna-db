4f616bff5a7738ef928c5c759e662be5993c7a8f
Kato Shinya <kato.shinya.dev@gmail.com>
2021-02-14 16:45:11 +0900

    update documentation

--- app/src/main/java/org/thinkit/zenna/exception/ContentParsingException.java
+++ app/src/main/java/org/thinkit/zenna/exception/ContentParsingException.java
@@ -58,8 +58,8 @@ public final class ContentParsingException extends RuntimeException {
      * @param message the detail message (which is saved for later retrieval by the
      *                {@link Throwable#getMessage()} method).
      * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link Throwable#getCause()} method). (A <tt>null</tt> value
-     *                is permitted, and indicates that the cause is nonexistent or
+     *                {@link Throwable#getCause()} method). (A nullvalue is
+     *                permitted, and indicates that the cause is nonexistent or
      *                unknown.)
      */
     public ContentParsingException(String message, Throwable cause) {
@@ -68,15 +68,14 @@ public final class ContentParsingException extends RuntimeException {
 
     /**
      * Constructs a new exception with the specified cause and a detail message of
-     * <tt>(cause==null ? null : cause.toString())</tt> (which typically contains
-     * the class and detail message of <tt>cause</tt>). This constructor is useful
-     * for exceptions that are little more than wrappers for other throwables (for
-     * example, {@link java.security.PrivilegedActionException}).
+     * (cause==null ? null : cause.toString())(which typically contains the class
+     * and detail message of cause. This constructor is useful for exceptions that
+     * are little more than wrappers for other throwables (for example,
+     * {@link java.security.PrivilegedActionException}).
      *
      * @param cause the cause (which is saved for later retrieval by the
-     *              {@link Throwable#getCause()} method). (A <tt>null</tt> value is
-     *              permitted, and indicates that the cause is nonexistent or
-     *              unknown.)
+     *              {@link Throwable#getCause()} method). (A nullvalue is permitted,
+     *              and indicates that the cause is nonexistent or unknown.)
      */
     public ContentParsingException(Throwable cause) {
         super(cause);
