2a4f8e1ec4e8ade11e5cfd25d74f6c5871890dd6
Kato Shinya <kato.shinya.dev@gmail.com>
2021-02-15 14:11:50 +0900

    add

---
+++ app/src/main/java/org/thinkit/zenna/util/InputStreamResolver.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.zenna.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * Provides operations on input stream.
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
+ */
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class InputStreamResolver {
+
+    /**
+     * Converts the input stream passed as argument to a string.
+     *
+     * @param inputStream The input stream to be converted
+     * @return The string obtained from the input stream
+     *
+     * @exception NullPointerException If {@code null} is passed as an argument
+     */
+    public static String toString(@NonNull final InputStream inputStream) {
+
+        final StringBuilder sequence = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sequence.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sequence.toString();
+    }
+}
